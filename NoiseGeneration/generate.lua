acd7c09450587c2cf55ea1dabd8deffa1d943e96
BrinkOfBaron <zachcbaron@gmail.com>
2026-09-07 14:00:53 -0400

    Finished initial Perlin Noise generation function

---
+++ NoiseGeneration/generate.lua
@@ -0,0 +1,35 @@
+--Generate.lua--
+Generate = {}
+
+function Generate:random_seeding()
+	love.math.setRandomSeed(os.time())
+	return love.math.random()
+end
+
+function Generate:matrix_init(size)
+	local MAP = {}
+	for i=1, size do
+		MAP[i] = {}
+		for z=1, size do
+			MAP[i][z] = love.math.noise(self:random_seeding(), self:random_seeding())	
+		end
+	end
+
+	return MAP
+end
+
+
+
+
+function debug_print(MAP, size)
+	for i=1, size do
+		print("\n")
+		for z=1, size do
+			print(MAP[i][z])
+		end
+	end
+end
+
+debug_print(Generate:matrix_init(8), 8) --Run as normal lua file--
+
+
